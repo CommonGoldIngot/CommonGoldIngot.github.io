f2f09502e49f8b264bf187436d38b8d52cbeda78
CommonGoldIngot <105542782+CommonGoldIngot@users.noreply.github.com>
2023-04-29 20:05:59 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ setTimeout(function () {
 }, 5000);
 //加载界面淡出
 function loaded() {
-    $("div.loading").fadeOut(1500, function () {
+    $("div.loading").fadeOut(500, function () {
         $("div.loading").remove();
     });
 }
@@ -15,21 +15,19 @@ $(document).ready(function () {
     $("div.aside-mask").click(function () {
         $("div.aside-sidebar, div.aside-mask").hide();
     });
-        //侧边栏子页面
-        $("li.aside-sidebar-item-has-subnav").click(function () {
-            $("ul.aside-subnav").slideToggle("fast", function () {
-                $("i.chevron-rotate").css("animation-name","chevron-rotate");
-                setTimeout(function () {
-                    $("i.chevron-rotate").removeClass("bi-chevron-right chevron-rotate").addClass("bi-chevron-down chevron-rotate-back");
-                    $("li.aside-sidebar-item-has-subnav").addClass("aside-sidebar-item-has-subnav-clicked");
-                }, 300);
-            });
-        });
-        $("li.aside-sidebar-item-has-subnav-clicked").click(function () {
-            $("i.chevron-rotate-back").css("animation-name","chevron-rotate-back");
-            setTimeout(function () {
-                $("i.chevron-rotate-back").removeClass("bi-chevron-down chevron-rotate-back").addClass("bi-chevron-right chevron-rotate");
-                $("li.aside-sidebar-item-has-subnav").removeClass("aside-sidebar-item-has-subnav-clicked");
-            }, 300);
-        });
+    //侧边栏子页面
+    $("li.aside-sidebar-item-has-subnav").rotate({
+        bind:
+        {
+            mouseover: function() {
+                $(".mdi-chevron-right").rotate({animateTo:90})
+            },
+            mouseout : function() {
+                $(".mdi-chevron-right").rotate({animateTo:0})
+            }
+        }
+    });
+    $("li.aside-sidebar-item-has-subnav").click(function () {
+        $("ul.aside-subnav").slideToggle("fast");
+    });
 });
