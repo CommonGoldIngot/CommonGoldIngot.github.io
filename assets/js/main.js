00acced08a43765b4ce628ceac98ef8e87166244
CommonGoldIngot <105542782+CommonGoldIngot@users.noreply.github.com>
2023-05-07 16:59:56 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,31 +9,37 @@ $(document).ready(function () {
     //侧边栏
     $("button.aside-unfold-sidebar").on("click", function () {
         $("div.aside-mask").show();
-        $("div.aside-sidebar").show().animate({left: "0"}, "fast");
+        $("div.aside-sidebar").show().animate({left: "0"}, 300);
     });
     $("div.aside-mask").click(function () {
         $("div.aside-mask").hide();
-        $("div.aside-sidebar").animate({left: "-300"}, "fast", function () {
+        $("div.aside-sidebar").animate({left: "-302"}, 300, function () {
             $("div.aside-sidebar").hide();
         });
     });
     //侧边栏子页面
     var wiki = 0;
-    var wikiR1 = function () { $("i.--wiki").rotate({animateTo: 90}); };
-    var wikiR2 = function () { $("i.--wiki").rotate({animateTo: 0}); };
-    var wikiS = function () { $("ul.--wiki").slideToggle("fast"); };
+    var wikiS = function () { $("ul.--wiki").slideToggle(300); };
+    var wikiR1 = function () { $("i.--wiki").rotate({duration: 300, animateTo: 90}); };
+    var wikiR2 = function () { $("i.--wiki").rotate({duration: 300, animateTo: 0}); };
     $("li.--wiki").on({
-        click: wikiS,
         mouseover: wikiR1,
-        mouseout: wikiR2
-    });
-    $("li.--wiki").on("click", function () {
-        if (wiki == 0) {
-            wikiR1;
-            wiki = 1;
-        } else {
-            wikiR2;
-            wiki = 0;
+        click: function () {
+            if (wiki == 0) {
+                wikiR1;
+                wiki = 1;
+            } else {
+                wikiR2;
+                wiki = 0;
+            }
+            wikiS;
+        },
+        mouseout: function () {
+            if (wiki == 1) {
+                wikiR1;
+            } else {
+                wikiR2;
+            }
         }
     });
 });
