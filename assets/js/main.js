970e3254155faf9770c103119c752ff9275c46f1
CommonGoldIngot <105542782+CommonGoldIngot@users.noreply.github.com>
2023-06-10 00:25:40 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
         var cardW4 = $("div.card-4").outerWidth();
         if (cardH1 - cardH2 < cardH3 && mainW - cardW1 - cardW2 < cardW3) {
             $("div.card-2").after("<samp></samp>");
-        } else if (cardH1 - cardH3 < cardH4 && cardH2 - cardH3 < cardH4 && mainW - cardW1 - cardW2 - cardW3 < cardW4) {
+        } else if (cardH2 - cardH3 < cardH4 && mainW - cardW1 - cardW2 - cardW3 < cardW4) {
             $("div.card-3").after("<samp></samp>");
         }
     }
