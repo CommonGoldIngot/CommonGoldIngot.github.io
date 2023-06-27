f7f47f100fb1d719ec341d96dc9f73a61261d7ba
CommonGoldIngot <105542782+CommonGoldIngot@users.noreply.github.com>
2023-06-28 06:06:58 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,11 +17,11 @@ setTimeout(function () {
     $("p.loading-text").text("就快好了，再等我一会儿...");
 }, 12000)
 setTimeout(function () {
-    $("p.loading-speed-up").css("visibility","visible");
+    $("p.loading-speed-up").css("visibility", "visible");
 }, 18000)
 //加载界面淡出 & 主题切换提示
 window.onload = function () {
-    setTimeout(function () { 
+    setTimeout(function () {
         $("p.loading-text").text("加载完成！");
         $("div.loading").fadeOut(500, function () {
             $("div.loading").remove();
@@ -30,7 +30,17 @@ window.onload = function () {
             if (20 <= hourForThemeControl || hourForThemeControl <= 5) {
                 setTimeout(function () {
                     $("div.aside-theme-control-tip").fadeIn(3500);
-                }, 1600)
+                }, 1400)
+                setTimeout(function () {
+                    $("div.aside-theme-control-tip").fadeOut(3500, function () {
+                        $("div.aside-theme-control-tip").remove();
+                    });
+                }, 3600)
+            } else if (window.matchMedia("(prefer-color-scheme: dark)").matches) {
+                $("span.aside-theme-control-tip-text").html("检测到您的浏览器已设置为<br/>暗色主题，已自动同步~<br/>您可以在此处切换主题~");
+                setTimeout(function () {
+                    $("div.aside-theme-control-tip").fadeIn(3500);
+                }, 1400)
                 setTimeout(function () {
                     $("div.aside-theme-control-tip").fadeOut(3500, function () {
                         $("div.aside-theme-control-tip").remove();
@@ -44,7 +54,46 @@ $(document).ready(function () {
     //主题切换
     $("button.aside-theme-control").on("click", function () {
         $("div.aside-theme-control-tip").remove();
-        $("div.aside-theme-select").slideToggle(500);
+        $("div.aside-theme-select").slideToggle(400);
+    });
+    $("li.--theme-auto").on("click", function () {
+        $("p.loading-text").text("正在切换主题...");
+        $("div.loading").show();
+        $("li.--theme-auto").css({"background-color": "rgb(var(--main-white))", "box-shadow": "9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))"});
+        $("i.--theme-auto").css("color", "rgb(var(--main-green))");
+        $("span.--theme-auto").css("color", "rgb(var(--main-green))");
+        window.onload = function () {
+            $("p.loading-text").text("加载完成！");
+            $("div.loading").fadeOut(500, function () {
+                $("div.loading").remove();
+            }
+        }
+    });
+    $("li.--theme-light").on("click", function () {
+        $("p.loading-text").text("正在切换主题...");
+        $("div.loading").show();
+        $("li.--theme-light").css({"background-color": "rgb(var(--main-white))", "box-shadow": "9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))"});
+        $("i.--theme-light").css("color", "rgb(var(--main-green))");
+        $("span.--theme-light").css("color", "rgb(var(--main-green))");
+        window.onload = function () {
+            $("p.loading-text").text("加载完成！");
+            $("div.loading").fadeOut(500, function () {
+                $("div.loading").remove();
+            }
+        }
+    });
+    $("li.--theme-dark").on("click", function () {
+        $("p.loading-text").text("正在切换主题...");
+        $("div.loading").show();
+        $("li.--theme-dark").css({"background-color": "rgb(var(--main-white))", "box-shadow": "9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))"});
+        $("i.--theme-dark").css("color", "rgb(var(--main-green))");
+        $("span.--theme-dark").css("color", "rgb(var(--main-green))");
+        window.onload = function () {
+            $("p.loading-text").text("加载完成！");
+            $("div.loading").fadeOut(500, function () {
+                $("div.loading").remove();
+            }
+        }
     });
     //侧边栏
     $("button.aside-unfold-sidebar").on("click", function () {
