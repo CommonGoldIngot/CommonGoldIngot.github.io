c9f4d6f36a14b43c15c9031c2b581c8857d9a3c4
CommonGoldIngot <105542782+CommonGoldIngot@users.noreply.github.com>
2023-07-16 07:02:01 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,21 +8,21 @@ setInterval(function () {
     $("img.loading-logo").rotate(logoRotateAngle);
 }, 5);
 setTimeout(function () {
-    $("p.loading-text").text("正在加载背景图片...");
+    $("p.loading-text").text('正在加载背景图片...');
 }, 3000)
 setTimeout(function () {
-    $("p.loading-text").text("加载背景图片可能需要较长时间，请耐心等待...");
+    $("p.loading-text").text('加载背景图片可能需要较长时间，请耐心等待...');
 }, 7000)
 setTimeout(function () {
-    $("p.loading-text").text("就快好了，再等我一会儿...");
+    $("p.loading-text").text('就快好了，再等我一会儿...');
 }, 12000)
 setTimeout(function () {
-    $("p.loading-speed-up").css("visibility", "visible");
+    $("p.loading-speed-up").css('visibility', 'visible');
 }, 18000)
 //加载界面淡出 & 主题切换提示
 window.onload = function () {
     setTimeout(function () {
-        $("p.loading-text").text("加载完成！");
+        $("p.loading-text").text('加载完成！');
         $("div.loading").fadeOut(500, function () {
             $("div.loading").hide();
             var timeForThemeControl = new Date();
@@ -36,8 +36,8 @@ window.onload = function () {
                         $("div.aside-theme-control-tip").remove();
                     });
                 }, 3600)
-            } else if (window.matchMedia("(prefer-color-scheme: dark)").matches) {
-                $("span.aside-theme-control-tip-text").html("检测到您的浏览器设置为<br/>深色主题，已自动同步~<br/>您可以在此处切换主题~");
+            } else if (window.matchMedia('(prefer-color-scheme: dark)').matches) {
+                $("span.aside-theme-control-tip-text").html('检测到您的浏览器设置为<br/>深色主题，已自动同步~<br/>您可以在此处切换主题~');
                 setTimeout(function () {
                     $("div.aside-theme-control-tip").fadeIn(3500);
                 }, 1400)
@@ -52,12 +52,12 @@ window.onload = function () {
 }
 $(document).ready(function () {
     //主题切换
-    $("button.aside-theme-control").on("click", function () {
+    $("button.aside-theme-control").on('click', function () {
         $("div.aside-theme-control-tip").remove();
         $("div.aside-theme-select").slideToggle(400);
     });
     function showLoadScreen() {
-        $("p.loading-text").text("正在切换主题...");
+        $("p.loading-text").text('正在切换主题...');
         $("p.loading-speed-up").hide();
         setTimeout(function () {
             $("div.loading").show();
@@ -65,38 +65,38 @@ $(document).ready(function () {
     }
     function closeLoadScreen() {
         setTimeout(function () {
-            $("p.loading-text").text("加载完成！");
+            $("p.loading-text").text('加载完成！');
             $("div.loading").fadeOut(500, function () {
                 $("div.loading").hide();
             });
         }, 2100)
     }
     function autoThemeSelected() {
-        $("li.--theme-auto").css({"background-color": "rgb(var(--main-white))", "box-shadow": "9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))"});
-        $("i.--theme-auto, span.--theme-auto").css("color", "rgb(var(--main-green))");
+        $("li.--theme-auto").css({'background-color': 'rgb(var(--main-white))', 'box-shadow': '9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))'});
+        $("i.--theme-auto, span.--theme-auto").css('color', 'rgb(var(--main-green))');
     }
     function lightThemeSelected() {
-        $("li.--theme-light").css({"background-color": "rgb(var(--main-white))", "box-shadow": "9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))"});
-        $("i.--theme-light, span.--theme-light").css("color", "rgb(var(--main-green))");
+        $("li.--theme-light").css({'background-color': 'rgb(var(--main-white))', 'box-shadow': '9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))'});
+        $("i.--theme-light, span.--theme-light").css('color', 'rgb(var(--main-green))');
     }
     function darkThemeSelected() {
-        $("li.--theme-dark").css({"background-color": "rgb(var(--main-white))", "box-shadow": "9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))"});
-        $("i.--theme-dark, span.--theme-dark").css("color", "rgb(var(--main-green))");
+        $("li.--theme-dark").css({'background-color': 'rgb(var(--main-white))', 'box-shadow': '9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))'});
+        $("i.--theme-dark, span.--theme-dark").css('color', 'rgb(var(--main-green))');
     }
     function autoThemeUnselected() {
-        $("li.--theme-auto").css({"background-color": "transparent", "box-shadow": "none"});
-        $("i.--theme-auto, span.--theme-auto").css("color", "rgb(var(--main-white))");
+        $("li.--theme-auto").css({'background-color': 'transparent', 'box-shadow': 'none'});
+        $("i.--theme-auto, span.--theme-auto").css('color', 'rgb(var(--main-white))');
     }
     function lightThemeUnselected() {
-        $("li.--theme-light").css({"background-color": "transparent", "box-shadow": "none"});
-        $("i.--theme-light, span.--theme-light").css("color", "rgb(var(--main-white))");
+        $("li.--theme-light").css({'background-color': 'transparent', 'box-shadow': 'none'});
+        $("i.--theme-light, span.--theme-light").css('color', 'rgb(var(--main-white))');
     }
     function darkThemeUnselected() {
-        $("li.--theme-dark").css({"background-color": "transparent", "box-shadow": "none"});
-        $("i.--theme-dark, span.--theme-dark").css("color", "rgb(var(--main-white))");
+        $("li.--theme-dark").css({'background-color': 'transparent', 'box-shadow': 'none'});
+        $("i.--theme-dark, span.--theme-dark").css('color', 'rgb(var(--main-white))');
     }
     var currentTheme = 0;
-    $("li.--theme-auto").on("click", function () {
+    $("li.--theme-auto").on('click', function () {
         if (currentTheme != 0) {
             showLoadScreen();
             autoThemeSelected();
@@ -104,11 +104,9 @@ $(document).ready(function () {
             darkThemeUnselected();
             closeLoadScreen();
             currentTheme = 0;
-        } else {
-            return;
         }
     });
-    $("li.--theme-light").on("click", function () {
+    $("li.--theme-light").on('click', function () {
         if (currentTheme != 1) {
             showLoadScreen();
             autoThemeUnselected();
@@ -116,11 +114,9 @@ $(document).ready(function () {
             darkThemeUnselected();
             closeLoadScreen();
             currentTheme = 1;
-        } else {
-            return;
         }
     });
-    $("li.--theme-dark").on("click", function () {
+    $("li.--theme-dark").on('click', function () {
         if (currentTheme != 2) {
             showLoadScreen();
             autoThemeUnselected();
@@ -128,43 +124,41 @@ $(document).ready(function () {
             darkThemeSelected();
             closeLoadScreen();
             currentTheme = 2;
-        } else {
-            return;
         }
     });
     $("li.--theme-auto").on("mouseenter", function () {
-        $("li.--theme-auto").css({"background-color": "rgb(var(--main-white))", "box-shadow": "9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))"});
-        $("i.--theme-auto, span.--theme-auto").css("color", "rgb(var(--main-green))");
+        $("li.--theme-auto").css({'background-color': 'rgb(var(--main-white))', 'box-shadow': '9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))'});
+        $("i.--theme-auto, span.--theme-auto").css('color', 'rgb(var(--main-green))');
     }).on("mouseleave", function () {
         if (currentTheme != 0) {
-            $("li.--theme-auto").css({"background-color": "transparent", "box-shadow": "none"});
-            $("i.--theme-auto, span.--theme-auto").css("color", "rgb(var(--main-white))");
+            $("li.--theme-auto").css({'background-color': 'transparent', 'box-shadow': 'none'});
+            $("i.--theme-auto, span.--theme-auto").css('color', 'rgb(var(--main-white))');
         }
     });
     $("li.--theme-light").on("mouseenter", function () {
-        $("li.--theme-light").css({"background-color": "rgb(var(--main-white))", "box-shadow": "9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))"});
-        $("i.--theme-light, span.--theme-light").css("color", "rgb(var(--main-green))");
+        $("li.--theme-light").css({'background-color': 'rgb(var(--main-white))', 'box-shadow': '9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))'});
+        $("i.--theme-light, span.--theme-light").css('color', 'rgb(var(--main-green))');
     }).on("mouseleave", function () {
         if (currentTheme != 1) {
-            $("li.--theme-light").css({"background-color": "transparent", "box-shadow": "none"});
-            $("i.--theme-light, span.--theme-light").css("color", "rgb(var(--main-white))");
+            $("li.--theme-light").css({'background-color': 'transparent', 'box-shadow': 'none'});
+            $("i.--theme-light, span.--theme-light").css('color', 'rgb(var(--main-white))');
         }
     });
     $("li.--theme-dark").on("mouseenter", function () {
-        $("li.--theme-dark").css({"background-color": "rgb(var(--main-white))", "box-shadow": "9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))"});
-        $("i.--theme-dark, span.--theme-dark").css("color", "rgb(var(--main-green))");
+        $("li.--theme-dark").css({'background-color': 'rgb(var(--main-white))', 'box-shadow': '9px 0px rgb(var(--main-white)),-9px 0px rgb(var(--main-white))'});
+        $("i.--theme-dark, span.--theme-dark").css('color', 'rgb(var(--main-green))');
     }).on("mouseleave", function () {
         if (currentTheme != 2) {
-            $("li.--theme-dark").css({"background-color": "transparent", "box-shadow": "none"});
-            $("i.--theme-dark, span.--theme-dark").css("color", "rgb(var(--main-white))");
+            $("li.--theme-dark").css({'background-color': 'transparent', 'box-shadow': 'none'});
+            $("i.--theme-dark, span.--theme-dark").css('color', 'rgb(var(--main-white))');
         }
     });
     //侧边栏
-    $("button.aside-unfold-sidebar").on("click", function () {
+    $("button.aside-unfold-sidebar").on('click', function () {
         $("div.aside-mask").show();
         $("div.aside-sidebar").show().animate({left: "0"}, 200);
     });
-    $("div.aside-mask").on("click", function () {
+    $("div.aside-mask").on('click', function () {
         $("div.aside-mask").hide();
         $("div.aside-sidebar").animate({left: "-302"}, 200, function () {
             $("div.aside-sidebar").hide();
@@ -172,7 +166,7 @@ $(document).ready(function () {
     });
     //侧边栏子页面操作
     var wiki = 0;
-    $("li.--wiki").on("click", function () {
+    $("li.--wiki").on('click', function () {
         $("ul.--wiki").slideToggle(200, function () {
             if (wiki == 0) {
                 $("i.--wiki").rotate({
