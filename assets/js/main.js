52a855cae1a9159a93200ea6891b81934ff3e238
CommonGoldIngot <105542782+CommonGoldIngot@users.noreply.github.com>
2023-08-05 23:02:27 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,15 @@ window.onload = function () {
         });
     }, 900)
 }
+//移动端屏幕转动时重载网页
+window.onorientationchange = function () {
+    $("p.loading-text").text('为了您的浏览体验，请不要频繁转动屏幕。');
+    $("div.loading").show(100, function () {
+        setTimeout(function () {
+            location.reload();
+        }, 1000)
+    });
+}
 $(document).ready(function () {
     //主题切换
     $("button.aside-theme-control").on('click', function () {
