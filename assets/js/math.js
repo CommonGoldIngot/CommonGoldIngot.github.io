077e6ebd35df6893e6ec8cf4d9dfc52faf178409
CommonGoldIngot <105542782+CommonGoldIngot@users.noreply.github.com>
2023-08-27 03:44:28 +0800

    Fix a bug: wrong " = " affect the function

--- assets/js/math.js
+++ assets/js/math.js
@@ -5,8 +5,8 @@ function answerFalse() {
     alert("答错力（悲");
 }
 $("input.math-submit-problem1").on('click', function () {
-    var answer1 = document.querySelector("input.math-input-text-problem1").value;
-    if (answer1 = "123/14") {
+    var answer1 = $("input.math-input-text-problem1").val();
+    if (answer1 == "123/14") {
         answerTrue();
     } else {
         answerFalse();
