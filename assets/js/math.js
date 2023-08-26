ba98015e221dc2de10281ca4c38048a9ac56de93
CommonGoldIngot <105542782+CommonGoldIngot@users.noreply.github.com>
2023-08-26 22:14:46 +0800

    Add files via upload

--- assets/js/math.js
+++ assets/js/math.js
@@ -2,13 +2,13 @@ function answerTrue() {
     alert("答对力（喜");
 }
 function answerFalse() {
-    alert("答错力（悲"):
+    alert("答错力（悲");
 }
-var answer1 = $("input.math-input-text-problem1").value;
-function checkAnswer1() {
+$("input.math-submit-problem1").on('click', function () {
+    var answer1 = document.querySelector("input.math-input-text-problem1").value;
     if (answer1 = "123/14") {
         answerTrue();
     } else {
         answerFalse();
     }
-}
+});
