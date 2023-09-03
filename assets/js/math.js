771847ce7107e9021ecf85e9b35708485ed9d7d9
CommonGoldIngot <105542782+CommonGoldIngot@users.noreply.github.com>
2023-09-03 21:37:08 +0800

    Add files via upload

--- assets/js/math.js
+++ assets/js/math.js
@@ -5,7 +5,7 @@ function answerFalse() {
     alert("答错力（悲");
 }
 $("input.math-submit-problem1").on('click', function () {
-    var answer1 = $("input.math-input-text-problem1").val();
+    var answer1 = document.querySelector("input.math-input-text-problem1").value.valueOf();
     if (answer1 == "123/14") {
         answerTrue();
     } else {
