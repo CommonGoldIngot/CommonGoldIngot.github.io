0ab4d8eed4ed92fef760c5b7cc1ef098359922f6
CommonGoldIngot <common_gold_ingot@qq.com>
2025-07-15 16:32:41 +0800

    Prefetching module test

--- assets/js/main-modules.js
+++ assets/js/main-modules.js
@@ -1,3 +1,12 @@
+//头部预提取文件
+$('link[href="/assets/css/main.css"]').before(`
+<link rel="prefetch" href="/assets/images/bg-light-landscape.png">
+<link rel="prefetch" href="/assets/images/bg-light-portrait.png">
+<link rel="prefetch" href="/assets/images/bg-dark-landscape.png">
+<link rel="prefetch" href="/assets/images/bg-dark-portrait.png">
+<link rel="prefetch" href="/assets/css/main-dark.css">
+`);
+//导航栏
 $(document).ready(function () {
     $("aside.navbar").load("/assets/modules/navbar.html", asideLoadedCallback);
 });
