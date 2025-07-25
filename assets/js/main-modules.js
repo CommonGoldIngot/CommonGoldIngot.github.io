0ea7b3ce15ecbe67b80b565c607b2f56c7662e30
CommonGoldIngot <common_gold_ingot@qq.com>
2025-07-25 19:18:39 +0800

    Update prefetch links

--- assets/js/main-modules.js
+++ assets/js/main-modules.js
@@ -1,11 +1,19 @@
 //头部预提取文件
-$('link[href="/assets/css/main.css"]').before(`
+var prefetchLinkContent = `
 <link rel="prefetch" href="/assets/images/bg-light-landscape.png">
 <link rel="prefetch" href="/assets/images/bg-light-portrait.png">
 <link rel="prefetch" href="/assets/images/bg-dark-landscape.png">
 <link rel="prefetch" href="/assets/images/bg-dark-portrait.png">
+<link rel="prefetch" href="/assets/css/main.css">
 <link rel="prefetch" href="/assets/css/main-dark.css">
-`);
+`;
+if ($('script[src="/assets/js/highlight-11.11.1.min.js"]').length) {
+    prefetchLinkContent += `
+    <link rel="prefetch" href="/assets/css/highlight-11.11.1-stackoverflow-light.min.css">
+    <link rel="prefetch" href="/assets/css/highlight-11.11.1-tokyo-night-dark.min.css">
+    `;
+}
+$('link[href="/assets/css/main.css"]').before(prefetchLinkContent);
 //导航栏
 $(document).ready(function () {
     $("aside.navbar").load("/assets/modules/navbar.html", asideLoadedCallback);
